184f0cecef270c96d9e4b14fbd5d7b0f51df4011
longsl <583297550@qq.com>
2018-08-29 19:33:25 +0800

    Site updated: 2018年08月29日 19:33:25

--- deploy-wiki.sh
+++ deploy-wiki.sh
@@ -12,18 +12,18 @@
 # Step 1
 cd /d/Wiki
 simiki g
-echo "[Step 1] hexo引擎已生成本地静态文件"
+echo "[Step 1] ······ hexo引擎已生成本地静态文件"
 
 # Step 2
 cd /d/Wiki/output
 git checkout master
 git commit -a -m "Site updated: `date`"    # Site updated: 2018年08月29日 15:39:17
 git push
-echo "[Step 2] Wiki站点静态文件已同步到git远程仓库的master分支"
+echo "[Step 2] ······ Wiki站点静态文件已同步到git远程仓库的master分支"
 
 # Step 3
 cd /d/Wiki
 git checkout dev
 git commit -a -m "Site updated: `date`"
 git push
-echo "[Step 3] Wiki站点源码已同步到git远程仓库的dev分支"
+echo "[Step 3] ······ Wiki站点源码已同步到git远程仓库的dev分支"
